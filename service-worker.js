7d9dfaad73c87dfe97348af81b747b9a7a32e98b
INeXS74 <cristi.ocolisan@gmail.com>
2026-03-17 00:06:10 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'airsoft-arbitraj-cache-v1';
+const urlsToCache = [
+  './', // Reprezintă index.html
+  'index.html',
+  'competitie_audio.wav',
+  'logo_federatie.png',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
